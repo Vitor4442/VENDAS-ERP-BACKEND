7875bc4b608246afa38cc1fa4b0d87a7701c353a
José Vitor <jose.vitorw018@gmail.com>
2025-11-21 15:12:06 -0300

    Feito o Atualizar Produto

--- src/main/java/io/github/dougllasfps/vendasapi/rest/produtos/ProdutoController.java
+++ src/main/java/io/github/dougllasfps/vendasapi/rest/produtos/ProdutoController.java
@@ -3,7 +3,12 @@ package io.github.dougllasfps.vendasapi.rest.produtos;
 import io.github.dougllasfps.vendasapi.model.Produto;
 import io.github.dougllasfps.vendasapi.model.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/produtos")
@@ -14,9 +19,23 @@ public class ProdutoController {
     private ProdutoRepository repository;
 
     @PostMapping
-    public ProdutoFormRequest salvar(@RequestBody ProdutoFormRequest produto){
+    public ProdutoFormRequest salvar(@RequestBody ProdutoFormRequest produto) {
         Produto instanciaProduto = produto.toModel();
         repository.save(instanciaProduto);
         return ProdutoFormRequest.fromModel(instanciaProduto);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity <Void> atualizar (@PathVariable Long id, @RequestBody ProdutoFormRequest produto){
+        Optional<Produto> produtoExistente = repository.findById(id);
+        if(produtoExistente.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+
+        Produto entidade = produto.toModel();
+        entidade.setId(id);
+        repository.save(entidade);
+
+        return ResponseEntity.ok().build();
     }
 }
