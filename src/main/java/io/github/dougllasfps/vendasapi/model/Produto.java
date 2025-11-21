1afb50315cd1fb44ad8866230e6df84450870ece
José Vitor <jose.vitorw018@gmail.com>
2025-11-21 12:04:04 -0300

    correcao de bugs

--- src/main/java/io/github/dougllasfps/vendasapi/model/Produto.java
+++ src/main/java/io/github/dougllasfps/vendasapi/model/Produto.java
@@ -1,66 +1,71 @@
 package io.github.dougllasfps.vendasapi.model;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
-import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-@Getter
-@Setter
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+
 @Entity
-@ToString
-@NoArgsConstructor
-@Table(name="produtos")
+@Table(name = "produtos")
 public class Produto {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "descricao",  length = 100)
-    private String descricao;
-
-    @Column(name = "nome",  length = 100)
+    @Column(name = "nome", length = 100)
     private String nome;
 
-    @Column(name = "preco",  precision = 16, scale = 2)
+    @Column(name = "descricao", length = 255)
+    private String descricao;
+
+    @Column(name = "preco", precision = 16, scale = 2)
     private BigDecimal preco;
+
+    @Column
+    private String sku;
 
     @Column(name = "data_cadastro")
     private LocalDate dataCadastro;
 
-    private String sku;
+    public Produto() {
+        super();
+    }
 
-    public Produto(Long id, String descricao, String nome, BigDecimal preco, String sku) {
+    public Produto(String nome, String descricao, BigDecimal preco, String sku) {
+        super();
+        this.nome = nome;
         this.descricao = descricao;
+        this.preco = preco;
+        this.sku = sku;
+    }
+
+    public Produto(Long id, String nome, String descricao, BigDecimal preco, String sku) {
+        super();
         this.id = id;
         this.nome = nome;
+        this.descricao = descricao;
         this.preco = preco;
         this.sku = sku;
     }
 
     @PrePersist
-    public void  prePersist(){
+    public void prePersist() {
         setDataCadastro(LocalDate.now());
     }
 
-    public String getSku() {
-        return sku;
+    public Long getId() {
+        return id;
     }
 
-    public void setSku(String sku) {
-        this.sku = sku;
-    }
-
-    public BigDecimal getPreco() {
-        return preco;
-    }
-
-    public void setPreco(BigDecimal preco) {
-        this.preco = preco;
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getNome() {
@@ -79,12 +84,20 @@ public class Produto {
         this.descricao = descricao;
     }
 
-    public Long getId() {
-        return id;
+    public BigDecimal getPreco() {
+        return preco;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
     }
 
     public LocalDate getDataCadastro() {
@@ -94,4 +107,11 @@ public class Produto {
     public void setDataCadastro(LocalDate dataCadastro) {
         this.dataCadastro = dataCadastro;
     }
+
+    @Override
+    public String toString() {
+        return "Produto [id=" + id + ", nome=" + nome + ", descricao=" + descricao + ", preco=" + preco + ", sku=" + sku
+                + "]";
+    }
+
 }
--- src/main/java/io/github/dougllasfps/vendasapi/rest/produtos/ProdutoFormRequest.java
+++ src/main/java/io/github/dougllasfps/vendasapi/rest/produtos/ProdutoFormRequest.java
@@ -1,19 +1,15 @@
 package io.github.dougllasfps.vendasapi.rest.produtos;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import io.github.dougllasfps.vendasapi.model.Produto;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-@Getter
-@Setter
-@ToString
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import io.github.dougllasfps.vendasapi.model.Produto;
+
 public class ProdutoFormRequest {
-    private long id;
+
+    private Long id;
     private String descricao;
     private String nome;
     private BigDecimal preco;
@@ -22,10 +18,13 @@ public class ProdutoFormRequest {
     @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate cadastro;
 
-    public ProdutoFormRequest(){
+    public ProdutoFormRequest() {
         super();
     }
-    public ProdutoFormRequest(long id, String descricao, String nome, BigDecimal preco, String sku, LocalDate cadastro) {
+
+    public ProdutoFormRequest(Long id, String descricao,
+                              String nome, BigDecimal preco, String sku, LocalDate cadastro) {
+        super();
         this.id = id;
         this.descricao = descricao;
         this.nome = nome;
@@ -34,12 +33,19 @@ public class ProdutoFormRequest {
         this.cadastro = cadastro;
     }
 
-    public static ProdutoFormRequest fromModel(Produto produto){
-        return new ProdutoFormRequest(produto.getId(), produto.getDescricao(), produto.getNome(), produto.getPreco(), produto.getSku(), produto.getDataCadastro());
+    public Produto toModel() {
+        return new Produto(id, nome, descricao, preco, sku);
     }
 
-    public Produto toModel(){
-        return new Produto(id, nome, descricao, preco, sku,);
+    public static ProdutoFormRequest fromModel(Produto produto) {
+        return new ProdutoFormRequest(
+                produto.getId(),
+                produto.getDescricao(),
+                produto.getNome(),
+                produto.getPreco(),
+                produto.getSku(),
+                produto.getDataCadastro()
+        );
     }
 
     public String getDescricao() {
@@ -74,11 +80,11 @@ public class ProdutoFormRequest {
         this.sku = sku;
     }
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
@@ -88,5 +94,11 @@ public class ProdutoFormRequest {
 
     public void setCadastro(LocalDate cadastro) {
         this.cadastro = cadastro;
+    }
+
+    @Override
+    public String toString() {
+        return "ProdutoFormRequest [id=" + id + ", descricao=" + descricao + ", nome=" + nome + ", preco=" + preco
+                + ", sku=" + sku + "]";
     }
 }
